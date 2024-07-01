135a548a92313cf78907226cf003349b1255c6d8
Mahdi Hajian <agentmahdihajian@gmail.com>
2024-07-01 15:26:31 +0330

    finish

--- ScrumAssistance-full.py
+++ ScrumAssistance-full.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import logging
-from telegram import Update
+from telegram import Update, User
 from telegram.constants import ParseMode
 from telegram.ext import (
     Application,
@@ -24,9 +24,10 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
-group_id = os.getenv('GTOUP_ID', -2163805514)  # Replace with your group chat ID
+group_id = os.getenv('GTOUP_ID', -1002163805514)  # Replace with your group chat ID
 topic_id = os.getenv('TOPIC_ID', 140)  # Replace with your group chat ID
 token = os.getenv('TOKEN', '7230447684:AAETM_yIJmnCCJQMjI7bZvZ3WNKosQQPnt4')
+all_users_id = [358434970, 266204483, 103275847, 1090118968]
 
 # Ensure the /data directory exists
 os.makedirs('data', exist_ok=True)
@@ -53,29 +54,25 @@ Session = sessionmaker(bind=engine)
 # Stages for conversation handler
 TASKS_TODAY, BLOCKERS, TASKS_TOMORROW = range(3)
 
-async def start(update: Update, context: CallbackContext) -> None:
-    await update.message.reply_text("Hi")
-
-
 async def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
-    await update.message.reply_text('Use /report to start your daily report.')
+    await update.message.reply_text('لطفا گزارش روزانه خود را با ارسال /report تکمیل کنید')
 
 async def start_report(update: Update, context: CallbackContext) -> int:
     """Start the daily report conversation."""
-    await update.message.reply_text('Today I worked on (list your tasks):')
+    await update.message.reply_text('امروز کار کردم روی:')
     return TASKS_TODAY
 
 async def tasks_today(update: Update, context: CallbackContext) -> int:
     """Handle the tasks worked on today."""
     context.user_data['tasks_today'] = update.message.text
-    await update.message.reply_text('Questions/Blockers (list any questions or blockers):')
+    await update.message.reply_text('سوال یا بلاکر:')
     return BLOCKERS
 
 async def blockers(update: Update, context: CallbackContext) -> int:
     """Handle the questions/blockers."""
     context.user_data['blockers'] = update.message.text
-    await update.message.reply_text('Tomorrow I will be working on (list your tasks):')
+    await update.message.reply_text('فردا قراره کار کنم روی:')
     return TASKS_TOMORROW
 
 async def tasks_tomorrow(update: Update, context: CallbackContext) -> int:
@@ -91,35 +88,41 @@ async def tasks_tomorrow(update: Update, context: CallbackContext) -> int:
         blockers=context.user_data['blockers'],
         tasks_tomorrow=context.user_data['tasks_tomorrow']
     )
+    session.query(Report).filter(Report.user_id == user_id).delete()
     session.add(new_report)
     session.commit()
     session.close()
 
-    await update.message.reply_text('Thank you for your report!')
+    await update.message.reply_text('تشکر بابت گزارش روزانه 🌹')
     return ConversationHandler.END
 
 async def cancel(update: Update, context: CallbackContext) -> int:
     """Cancel the conversation."""
-    await update.message.reply_text('Daily report canceled.')
+    await update.message.reply_text('درخواست گزارش شما لغو شد.')
     return ConversationHandler.END
+
+async def get_daily_report_message(context, reports) -> String:
+    report_text = "گزارشات روزانه:\n\n"
+    for report in reports:
+        user_link = await get_user_mention_by_user_id(context, report.user_id)
+        report_text += (
+            f"گزارش {user_link}:\n"
+            f"دیروز کار کرده روی:\n{report.tasks_today}\n"
+            f"سوال/ بلاکر:\n{report.blockers}\n"
+            f"امروز قراره کار کنه روی:\n{report.tasks_tomorrow}\n"
+            f"- - - - - - - -\n"
+        )
+    return report_text
+
 
 async def send_daily_report(context: CallbackContext) -> None:
     """Send daily report to the group."""
-    logger.info("send_daily_report works")
     session = Session()
     reports = session.query(Report).all()
     session.close()
 
     if reports:
-        report_text = "Daily Reports:\n\n"
-        for report in reports:
-            report_text += (
-                f"Report from user ID {report.user_id}:\n"
-                f"Today I worked on:\n{report.tasks_today}\n\n"
-                f"Questions/Blockers:\n{report.blockers}\n\n"
-                f"Tomorrow I will be working on:\n{report.tasks_tomorrow}\n\n"
-            )
-        
+        report_text = await get_daily_report_message(context, reports)
         await context.bot.send_message(group_id, message_thread_id=topic_id, text=report_text, parse_mode=ParseMode.MARKDOWN)
 
         # Delete all reports after sending
@@ -131,55 +134,66 @@ async def send_daily_report(context: CallbackContext) -> None:
 async def ask_for_daily_tasks(context: CallbackContext) -> None:
     """Send notification to group to send daily tasks."""
 
-    await context.bot.send_message(group_id, message_thread_id=topic_id, text="Please submit your daily tasks using /report.")
+    await context.bot.send_message(chat_id=group_id, message_thread_id=topic_id, text="لطفا گزارش روزانه خود را با ارسال /report تکمیل کنید")
 
 
 async def remind_users_to_send_tasks(context: Application) -> None:
     """Send reminder to users who haven't sent their tasks."""
     session = Session()
-    all_users = await context.bot.get_chat_members(context.job.context)
-    incomplete_reports = session.query(Report.user_id).filter(Report.tasks_today == None).all()
+    incomplete_reports = session.query(Report.user_id).all()
     session.close()
 
-    if all_users and incomplete_reports:
+    if all_users_id:
         # Extract user IDs from the query results
         users_with_reports = {report.user_id for report in incomplete_reports}
+        users_to_remind = []
+
         # Loop through all users in the group
-        for user in all_users:
-            user_id = user.user.id
+        for one_user in all_users_id:
+            user = await context.bot.get_chat(one_user)
+            user_id = user.id
             # Check if user hasn't submitted a report
             if user_id not in users_with_reports:
-                # Send reminder to user
-                await context.bot.send_message(user_id, text="Please submit your daily tasks using /report.")
+                users_to_remind.append(user)
+        
+        if users_to_remind:
+            # Create a message mentioning all users who need to submit their report
+            mentions = [get_user_mention_by_user(user)+'\n' for user in users_to_remind]
+            reminder_text = "لطفا گزارش روزانه خود را با ارسال /report تکمیل کنید\n" + " ".join(mentions)
+
+            # Send the message to the specified group and topic
+            await context.bot.send_message(
+                chat_id=group_id,
+                message_thread_id=topic_id,
+                text=reminder_text,
+                parse_mode=ParseMode.MARKDOWN
+            )
+
+async def get_user_info(context, user_id) -> User:
+    return await context.bot.get_chat(user_id)
+
+async def get_user_mention_by_user_id(context, user_id) -> User:
+    user = await get_user_info(context, user_id)
+    return get_user_mention_by_user(user)
+
+def get_user_mention_by_user(user: User) -> User:
+    mention = (f"[@{user.username}]" if False else f"[{user.first_name}]") + f'(tg://user?id={user.id})'
+    return mention
 
 async def send_daily_reports(update: Update, context: CallbackContext) -> None:
     """Command handler to manually fetch and send daily reports."""
     # Notify the user that reports are being fetched
-    await update.message.reply_text("Fetching daily reports...")
+    await update.message.reply_text("در حال دریافت گزارشات ...")
 
     session = Session()
     reports = session.query(Report).all()
     session.close()
 
     if reports:
-        report_text = "Daily Reports:\n\n"
-        for report in reports:
-            user = await context.bot.get_chat(report.user_id)
-            first_name = user.first_name if user.first_name else "Unknown"
-            last_name = user.last_name if user.last_name else ""
-            profile_link = f"[{first_name} {last_name}](https://t.me/{user.username})"
-
-            report_text += (
-                f"Report from {profile_link}:\n"
-                f"Today I worked on:\n{report.tasks_today}\n\n"
-                f"Questions/Blockers:\n{report.blockers}\n\n"
-                f"Tomorrow I will be working on:\n{report.tasks_tomorrow}\n\n"
-            )
-
-        # Send the accumulated reports
+        report_text = await get_daily_report_message(context, reports)
         await update.message.reply_text(report_text, parse_mode=ParseMode.MARKDOWN)
     else:
-        await update.message.reply_text("No daily reports found.")
+        await update.message.reply_text("هیچ گزارشی یافت نشد.")
     
     # Delete all reports after sending
     session = Session()
@@ -195,11 +209,9 @@ def schedule_jobs(application: Application) -> None:
     tz = pytz.timezone('Asia/Tehran')
 
     # Schedule daily tasks
-    job_queue.run_daily(send_daily_report, time=time(hour=9, minute=0, tzinfo=tz), days=(0, 1, 2, 3, 5, 6))
     job_queue.run_daily(ask_for_daily_tasks, time=time(hour=16, minute=0, tzinfo=tz), days=(0, 1, 2, 3, 5, 6))
     job_queue.run_daily(remind_users_to_send_tasks, time=time(hour=18, minute=0, tzinfo=tz), days=(0, 1, 2, 3, 5, 6))
-        
-    # job_queue.run_repeating(ask_for_daily_tasks, interval=7, first=0)
+    job_queue.run_daily(send_daily_report, time=time(hour=9, minute=0, tzinfo=tz), days=(0, 1, 2, 3, 5, 6))
     
 def main() -> None:
     """Start the bot."""
@@ -223,9 +235,6 @@ def main() -> None:
 
     application.add_handler(conv_handler)
     
-    # on different commands - answer in Telegram
-    # application.add_handler(CommandHandler("start", start))
-    # application.add_handler(CommandHandler("help", help_command))
     # Schedule the daily tasks
     schedule_jobs(application)
 
